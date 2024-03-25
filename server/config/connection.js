2f160f6dc3eb0e02741c77197c85da7799a2937a
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-24 23:07:39 -0500

    Co-authored-by: Patric Xiong <PatricXiong@users.noreply.github.com> Co-authored-by: Vincent Teune <cobalt88@users.noreply.github.com> Co-authored-by: seanharrigan8 <seanharrigan8@users.noreply.github.com>

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect( 'mongodb://127.0.0.1:27017/p3t2');
 
 module.exports = mongoose.connection;
+// process.env.MONGODB_URI ||
