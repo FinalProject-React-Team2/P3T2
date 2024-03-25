217d6549a4204e1f4ad18d03ce674eecd4db84b1
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-25 18:51:40 -0500

    resolving merge

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,14 +4,8 @@ const typeDefs = `
     firstName: String
     lastName: String
     email: String
-<<<<<<< HEAD
     debates: [Debate]
    
-=======
-    openDebates: [Debate]
-    activeDebates: [Debate]
-    closedDebates: [Debate]
->>>>>>> aff4ff3 (safety commit)
   }
   type Debate {
     _id: ID
@@ -34,16 +28,6 @@ const typeDefs = `
 
   type Comment {
     user: String
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      type: String
->>>>>>> 1de947d (Keep calm and carry on boys)
-=======
-      type: String
-=======
->>>>>>> 10b6de8 (fixed all backend wiring, updated User model,  added getUserDebates query, fixed createDebate resolver path.)
->>>>>>> aff4ff3 (safety commit)
     comment: String
   }
 
@@ -56,10 +40,7 @@ const typeDefs = `
   type Query {
     user(userId: ID!): User
     users: [User]
-<<<<<<< HEAD
     myProfile: User
-=======
->>>>>>> aff4ff3 (safety commit)
     getUserDebates: [User]
     getDebate(_id: ID!): Debate
     getDebates: [Debate]
@@ -67,23 +48,13 @@ const typeDefs = `
 
   input DebateInput {
     title: String!
-    createdBy: ID!
   } 
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     login(email: String!, password: String!): Auth
-<<<<<<< HEAD
     createDebate(debate: DebateInput!): Debate
-=======
-<<<<<<< HEAD
-
-    createDebate(title: String!): Debate
-=======
-    createDebate(debate: DebateInput!): Debate
->>>>>>> 10b6de8 (fixed all backend wiring, updated User model,  added getUserDebates query, fixed createDebate resolver path.)
->>>>>>> aff4ff3 (safety commit)
   }
 `;
 
