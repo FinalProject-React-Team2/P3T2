d8f4fa5c782b90ab6cc3d18c2e7254bef500a174
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-26 09:50:41 -0500

    resolving merge

--- client/src/pages/About/Aboutus.jsx
+++ client/src/pages/About/Aboutus.jsx
@@ -1,7 +1,6 @@
 //create an about us page where we just talk about our mission behind debate bros. make it simple and export it
 
 import React from "react";
-<<<<<<< HEAD
 import './aboutus.css';
 
 function About() {
@@ -14,20 +13,6 @@ function About() {
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
                     <div className="data">
         <a href="#" className="hire">Hire Me</a>
-=======
-// import About from '../About/Aboutus';
-
-function About() {
-    return (
-        <div class="about-us">
-            <div class="about">
-                <div class="text">
-                    <h2>About Us</h2>
-                        <h5>Front-end Developer & <span>Designer</span></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
-                    <div class="data">
-        <a href="#" class="hire">Hire Me</a>
->>>>>>> 5f07b57 (resolving merge)
         </div>
       </div>
     </div>
