dbc5433fd0723b30fadaa7bc380c39728cef0b80
Stuart Osborn <stuartgosborn22@gmail.com>
2024-03-29 18:49:46 -0500

    finsihed styling the debate room

--- client/src/pages/DebateRoom/index.jsx
+++ client/src/pages/DebateRoom/index.jsx
@@ -1,11 +1,6 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_DEBATE } from "../../utils/queries";
-import { ADD_OPPONENT } from "../../utils/mutations";
-
-
-import AuthService from "../../utils/auth";
 import Grid from "@mui/material/Unstable_Grid2";
 import DebateInputs from "../../components/DebateRoom/DebateInputs";
 
@@ -26,7 +21,11 @@ function DebateRoom() {
 
   return (
     <>
-      <div className="container">
+    <Grid container spacing={1}>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12} key={11}>
+
+        
+      <div className="container debateHeader">
         <div>
           <h2>{debate.title}</h2>
           <p>Challenger: {debate?.createdBy?.firstName || "error"}</p>
@@ -34,21 +33,10 @@ function DebateRoom() {
             Opponent: {debate?.opponent?.firstName || "waiting for opponent..."}
           </p>
           <p>{debate.numOfRounds} Rounds</p>
-          {/* Display debate arguments, speakers, etc. */}
-
-          {/* Comment Form */}
-          {/* <form>
-          <textarea></textarea>
-          <button type="submit">Add Comment</button>
-        </form> */}
-
-          {/* Vote Form */}
-          {/* <form>
-          <input type="number" />
-          <button type="submit">Vote</button>
-        </form> */}
         </div>
       </div>
+      </Grid>
+    </Grid>
       <DebateInputs debate={debate} id={debate._id} />
       {console.log("D.id" , debate._id)};
       
