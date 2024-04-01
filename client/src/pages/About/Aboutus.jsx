fe0d53080fae22d8ad0a5384610a8109b3c90834
Patric Xiong <pathlub@gmail.com>
2024-04-01 19:19:21 -0400

    update

--- client/src/pages/About/Aboutus.jsx
+++ client/src/pages/About/Aboutus.jsx
@@ -5,11 +5,15 @@ import './aboutus.css';
 function About() {
     return (
         <div className="about-us">
+             <div class="mission">
+        <h2>Our Mission</h2>
+        <p>At DebateCloud, we believe in the power of debate to inspire change, foster understanding, and build communities. Our mission is to provide a user-friendly, accessible platform where anyone, anywhere, can participate in meaningful debates on a wide range of topics. The Triumvirate is a passionate, three person team dedicated to bringing the best of digital debates into homes everywhere.
+        </p></div>
             {/* <h1>About Us</h1> */}
             <div className="about">
                 <div className="text">
                     <h2>Sean</h2>
-                        <p>The Triumvirate is a passionate, three person team dedicated to bringing the best of digital debates into homes everywhere. The team is made up Stuart "Big O" Osborn, Patric "The Star" Ziong, and Sean "no nickname" Harrigan.</p>
+                        <p>"Short Bio about yourself".</p>
                     <div className="data">
                     <img src="/images/sean.png" alt="Sean Harrigan" className="profile-image"></img>
         <a href="https://www.linkedin.com/in/sean-harrigan8/" className="hire">LinkedIn</a>
@@ -20,7 +24,7 @@ function About() {
                 <div className="about">
                 <div className="text">
                     <h2>Stuart</h2>
-                        <p>The Triumvirate is a passionate, three person team dedicated to bringing the best of digital debates into homes everywhere. The team is made up Stuart "Big O" Osborn, Patric "The Star" Ziong, and Sean "no nickname" Harrigan.</p>
+                        <p>"Short Bio about yourself".</p>
                     <div className="data">
                     <img src="/images/stuart.png" alt="Stuart" className="profile-image"></img>
         <a href="https://www.linkedin.com/in/stuartgosborn/" className="hire">LinkedIn</a>
@@ -31,7 +35,7 @@ function About() {
                 <div className="about">
                 <div className="text">
                     <h2>Patric</h2>
-                        <p>The Triumvirate is a passionate, three person team dedicated to bringing the best of digital debates into homes everywhere. The team is made up Stuart "Big O" Osborn, Patric "The Star" Ziong, and Sean "no nickname" Harrigan.</p>
+                        <p>"Short Bio about yourself".</p>
                     <div className="data">
                     <img src="/images/patric.png" alt="Patric" className="profile-image"></img>
         <a href="https://www.linkedin.com/in/patric-xiong-80a383284" className="hire">LinkedIn</a>
@@ -39,6 +43,10 @@ function About() {
                     </div>
                 </div>
                 </div>
+                <div class="promise">
+        <h2>Our Promise</h2>
+        <p>As we continue to grow, we promise to stay true to our core values of inclusivity, innovation, and integrity. Join us on this journey, share your voice, and let's make history together.</p>
+    </div>
         </div>
     
         
