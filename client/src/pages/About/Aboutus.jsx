a6ca65e7baacc08c1131a9ee2db099978c6b974d
Stuart Osborn <stuartgosborn22@gmail.com>
2024-04-01 18:48:40 -0500

    resolving bugs in aboutus

--- client/src/pages/About/Aboutus.jsx
+++ client/src/pages/About/Aboutus.jsx
@@ -5,7 +5,7 @@ import './aboutus.css';
 function About() {
     return (
         <div className="about-us">
-             <div class="mission">
+             <div className="mission">
         <h2>Our Mission</h2>
         <p>At DebateCloud, we believe in the power of debate to inspire change, foster understanding, and build communities. Our mission is to provide a user-friendly, accessible platform where anyone, anywhere, can participate in meaningful debates on a wide range of topics. The Triumvirate is a passionate, three person team dedicated to bringing the best of digital debates into homes everywhere.
         </p></div>
